6e631b56ff8e72caa05c04257efc914a7c3688a1
Patrick Thomson <patrickt@github.com>
2021-08-26 10:31:28 -0400

    Release 0.1.0.0.

--- src/Numeric/Parse/Flexible.hs
+++ src/Numeric/Parse/Flexible.hs
@@ -1,19 +1,26 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 
--- | Flexible numeric parsers for real-world programming languages. These parsers aim to be a superset of
--- the numeric syntaxes across the most popular programming languages.
+-- | Flexible numeric parsers for real-world programming languages.
+-- These parsers aim to be a superset of the numeric syntaxes across
+-- the most popular programming languages.
 --
--- All parsers assume any trailing whitespace has already been consumed, and places no requirement for an
--- @endOfInput@ at the end of a literal. Be sure to handle these in a calling context. These parsers do
--- not use 'TokenParsing', and therefore may fail while consuming input, depending on if you use a parser
+-- All parsers assume any trailing whitespace has already been
+-- consumed, and places no requirement for an @endOfInput@ at the end
+-- of a literal. Be sure to handle these in a calling context. These
+-- parsers do not use 'Text.Parser.Token.TokenParsing', and therefore
+-- may fail while consuming input, depending on if you use a parser
 -- that automatically backtracks or not. Apply 'try' if needed.
 module Numeric.Parse.Flexible
   ( integer,
+    natural,
+    decimal,
     hexadecimal,
     octal,
     binary,
     floating,
+    signed,
+    imaginary,
   )
 where
 
@@ -25,8 +32,10 @@ import Text.Parser.Char
 import Text.Parser.Combinators
 import Text.Read (readMaybe)
 import Prelude hiding (exponent, fail, takeWhile)
+import Data.Complex
+import Numeric.Natural (Natural)
 
--- | Parse an integer in 'decimal', 'hexadecimal', 'octal', or 'binary'.
+-- | Parse an integer in 'decimal', 'hexadecimal', 'octal', or 'binary', with optional leading sign.
 --
 -- Note that because the 'octal' parser takes primacy over 'decimal', numbers with a leading
 -- @0@ will be parsed as octal. This is unfortunate, but matches the behavior
@@ -34,9 +43,14 @@ import Prelude hiding (exponent, fail, takeWhile)
 integer :: (CharParsing m, Monad m) => m Integer
 integer = signed (choice [try hexadecimal, try octal, try binary, decimal])
 
+-- | Parse a natural number in 'decimal', 'hexadecimal', 'octal', or 'binary'. As with 'integer',
+-- a leading @0@ is interpreted as octal. Leading signs are not accepted.
+natural :: (CharParsing m, Monad m) => m Natural
+natural = fromIntegral <$> choice [try hexadecimal, try octal, try binary, decimal]
+
 -- | Parse an integer in base 10.
 --
--- Accepts @0..9@ and underscore separators.
+-- Accepts @0..9@ and underscore separators. No leading signs are accepted.
 decimal :: (CharParsing m, Monad m) => m Integer
 decimal = do
   contents <- withUnder digit
@@ -44,7 +58,7 @@ decimal = do
 
 -- | Parse a number in hexadecimal.
 --
--- Requires a @0x@ or @0X@ prefix.
+-- Requires a @0x@ or @0X@ prefix. No leading signs are accepted.
 -- Accepts @A..F@, @a..f@, @0..9@ and underscore separators.
 hexadecimal :: forall a m. (Eq a, Num a, CharParsing m, Monad m) => m a
 hexadecimal = do
@@ -58,7 +72,7 @@ hexadecimal = do
 
 -- | Parse a number in octal.
 --
--- Requires a @0@, @0o@ or @0O@ prefix.
+-- Requires a @0@, @0o@ or @0O@ prefix. No leading signs are accepted.
 -- Accepts @0..7@ and underscore separators.
 octal :: forall a m. (Num a, CharParsing m, Monad m) => m a
 octal = do
@@ -68,7 +82,7 @@ octal = do
 
 -- | Parse a number in binary.
 --
--- Requires a @0b@ or @0B@ prefix.
+-- Requires a @0b@ or @0B@ prefix. No leading signs are accepted.
 -- Accepts @0@, @1@, and underscore separators.
 binary :: forall a m. (Show a, Num a, CharParsing m, Monad m) => m a
 binary = do
@@ -117,11 +131,21 @@ floating = signed (choice [hexadecimal, octal, binary, dec])
       let trail = if null trailings then "0" else trailings
       attempt (leads <> "." <> trail <> exponent)
 
-signed :: (CharParsing m, Num a) => m a -> m a
+-- | Converts a numeric parser to one that accepts an optional leading sign.
+signed :: forall a m . (CharParsing m, Num a) => m a -> m a
 signed p =
   (negate <$> (char '-' *> p))
     <|> (char '+' *> p)
     <|> p
+
+-- | Converts a numeric parser to one that accepts a trailing imaginary specifier
+-- @i@ or @j@. This does not add facilities for two-valued literals, i.e. @1+4j@,
+-- as those are generally best left to high-level expression facilities.
+imaginary :: forall a m . (CharParsing m, Monad m, Num a) => m a -> m (Complex a)
+imaginary num = do
+  real <- num
+  void (oneOf "ij")
+  pure (0 :+ real)
 
 stripUnder :: String -> String
 stripUnder = Prelude.filter (/= '_')
